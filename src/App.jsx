4ef021c3915f54a65b0d43225b03de2088394ef5
Islam <islamderrouiche@gmail.com>
2025-12-24 08:18:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <form>
+          <label htmlFor="">Titre</label>
+          <input type="text" />
+
+          <label htmlFor="">Description</label>
+          <textarea name="" id=""></textarea>
+        </form>{" "}
+      </div>
+    </>
+  );
+}
+
+export default App;
